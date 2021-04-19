35ce7e78838e0a01865270c1f23a64ba99c660c8
Manthan Sharma <Sharmamanthan474@gmail.com>
2021-04-19 18:11:34 +0530

    Update MoleculeIDE.py

--- MoleculeIDE.py
+++ MoleculeIDE.py
@@ -3,7 +3,7 @@ from tkinter.filedialog import asksaveasfilename, askopenfilename
 import subprocess
 
 compiler = Tk()
-compiler.title('My Fantastic IDE')
+compiler.title('My Molecule IDE')
 file_path = ''
 
 
